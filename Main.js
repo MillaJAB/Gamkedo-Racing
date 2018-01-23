a5bc68feb6ff87e789baaf0760d1b42025dedf6f
Jenna Johnson <MillaJAB@users.noreply.github.com>
2018-01-22 19:11:45 -0600

    Images now need to load before game kicks off

--- Main.js
+++ Main.js
@@ -3,13 +3,15 @@ var canvas, canvasContext;
 window.onload = function() {
 	canvas = document.getElementById('gameCanvas');
 	canvasContext = canvas.getContext('2d');
-	
+
+	loadImages();
+}
+
+function imageLoadingDoneSoStartGame() {
 	var framesPerSecond = 30;
 	setInterval(updateAll, 1000/framesPerSecond);
 
 	setupInput();
-
-	loadImages();
 	carReset();
 }
 
